3f51f2b93657e1807c17ff97aeff0bd9b76ff06a
Noah Bean <92399044+noahbean33@users.noreply.github.com>
2025-11-16 19:26:36 -0800

    Add core compiler source files and update build
    Added major source files for compiler components including array, codegen, datatype, expressionable, fixup, helper, node, parser, preprocessor, rdefault, resolver, scope, stackframe, symresolver, and validator. Updated Makefile to include new object files and build rules. Expanded README with detailed course and project information. Added .vscode/settings.json for editor configuration and placeholder .keep files for build and include directories.

--- cprocess.c
+++ cprocess.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "compiler.h"
-struct compile_process *compile_process_create(const char *filename, const char *filename_out, int flags)
+#include "helpers/vector.h"
+
+const char* default_include_dirs[] = {"./pc_includes", "../pc_includes", "/usr/include/peach-includes", "/usr/include"};
+
+const char* compiler_include_dir_begin(struct compile_process* process)
+{
+    vector_set_peek_pointer(process->include_dirs, 0);
+    const char* dir = vector_peek_ptr(process->include_dirs);
+    return dir;
+}
+
+const char* compiler_include_dir_next(struct compile_process* process)
+{
+    const char* dir =vector_peek_ptr(process->include_dirs);
+    return dir;
+}
+
+void compiler_setup_default_include_directories(struct vector* include_vec)
+{
+    size_t total = sizeof(default_include_dirs) / sizeof(const char*);
+    for (int i = 0; i < total; i++)
+    {
+        vector_push(include_vec, &default_include_dirs[i]);
+    }
+}
+
+struct compile_process *compile_process_create(const char *filename, const char *filename_out, int flags, struct compile_process* parent_process)
 {
     FILE *file = fopen(filename, "r");
     if (!file)
@@ -20,9 +46,37 @@ struct compile_process *compile_process_create(const char *filename, const char 
     }
 
     struct compile_process* process = calloc(1, sizeof(struct compile_process));
+    process->token_vec = vector_create(sizeof(struct token));
+    process->token_vec_original = vector_create(sizeof(struct token));
+    process->node_vec = vector_create(sizeof(struct node*));
+    process->node_tree_vec = vector_create(sizeof(struct node*));
+    
     process->flags = flags;
     process->cfile.fp = file;
     process->ofile = out_file;
+    process->generator = codegenerator_new(process);
+    process->resolver = resolver_default_new_process(process);
+
+    symresolver_initialize(process);
+    symresolver_new_table(process);
+    
+    if (parent_process)
+    {
+        process->preprocessor = parent_process->preprocessor;
+        process->include_dirs = parent_process->include_dirs;
+    }
+    else
+    {
+        process->preprocessor = preprocessor_create(process);
+        process->include_dirs = vector_create(sizeof(const char*));
+        // Load the default include directories
+        compiler_setup_default_include_directories(process->include_dirs);
+    }
+    
+    char* path = malloc(PATH_MAX);
+    realpath(filename, path);
+    process->cfile.abs_path = path;
+    node_set_vector(process->node_vec, process->node_tree_vec);
     return process;
 }
 
--- test.c
+++ test.c
@@ -1,4 +1,17 @@
-gerog erlgermo 
-skgm5845 
-int 
-long 
+
+
+struct abc
+{
+    int a;
+    int b;
+};
+
+struct abc
+{
+
+};
+
+void test(int a, int b)
+{
+   return;
+}
